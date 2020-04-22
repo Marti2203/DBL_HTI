41873006ea3135a43ea19282f5fb769c854abc00
Marti2203 <marti_2203@abv.bg>
2020-04-22 10:59:51 +0200

    Add vue.js example
    Alex needs to check this

--- app.py
+++ app.py
@@ -7,7 +7,20 @@ def hello_world():
 <title>Base page</title>
 <h1>Hi!</h1>
 <p>This is a very basic front end</p>
-<script> alert('Hello there!')</script>
+<div id="app">
+  {{ message }}
+</div>
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+<script>
+"use strict";
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue from our DBL project!'
+  }
+});
+setTimeout(function (){ app.message="2 seconds passed"; console.log("2 seconds")},3000);
+</script>
 '''
 @app.route('/math')
 def math():
