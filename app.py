cebb25c86da7dff67f82e7eb3902817dba8fdc4c
Martin Mirchev <marti_2203@abv.bg>
2020-04-26 22:19:19 +0200

    Formatted documents, removed templates, added some vue routing

--- app.py
+++ app.py
@@ -1,27 +1,22 @@
 from flask import Flask, render_template, request
 app = Flask(__name__, static_folder="static")
 
+visualizations = [
+    {'name': 'Visualization 1', 'link': 'vis1'},
+    {'name': 'Visualization 2', 'link': 'vis2'},
+]
+
 @app.route('/')
 def main():
-    return render_template('helloworld.html')
+    return render_template('helloworld.html',
+                           visualizations=visualizations)
+
 
 @app.route('/upload')
 def upload():
     return render_template('upload.html')
 
+
 @app.route('/vis1')
 def vis1():
     return render_template('vis1.html')
-
-@app.route('/vis2')
-def vis2():
-    return render_template('vis2.html')
-
-@app.route('/vis3')
-def vis3():
-    return render_template('vis3.html')
-
-@app.route('/vis4')
-def vis4():
-    return render_template('vis4.html')
-
--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,20 @@
 "use strict";
-var app = new Vue({
-    el: '#app',
-    data: {
-    }
-});
-console.log("Hello")
+const Vis1 = { template: '<div>vis 1</div>' }
+const Vis2 = { template: '<div>vis 2</div>' }
+
+// 2. Define some routes
+// Each route should map to a component. The "component" can
+// either be an actual component constructor created via
+// `Vue.extend()`, or just a component options object.
+// We'll talk about nested routes later.
+const routes = [
+  { path: '/vis1', component: Vis1 },
+  { path: '/vis2', component: Vis2 }
+]
+
+const router = new VueRouter({
+    routes // short for `routes: routes`
+})
+const app = new Vue({
+    router
+}).$mount('#app')
