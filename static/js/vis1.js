fe1c0b421c98e972328f2b2e71b9af706a52e84d
Martin Mirchev <marti_2203@abv.bg>
2020-04-27 15:50:32 +0200

    Fixed up the tooltip, removed axis, made d3 a base requirement.

--- static/js/vis1.js
+++ static/js/vis1.js
@@ -1,11 +1,10 @@
 'use strict';
 
 function toHex(n) {
-    var hex = Number(n).toString(16);
+    let hex = Number(n).toString(16);
     if (hex.length < 2) {
         hex = "0" + hex;
     }
-    console.log(hex);
     return hex;
 };
 // set the dimensions and margins of the graph
@@ -14,7 +13,7 @@ const margin = { top: 10, right: 30, bottom: 30, left: 60 },
     height = 1200 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
-var svg = d3.select("#scatter-plot")
+const svg = d3.select("#scatter-plot")
     .append("svg")
     .style("background-image", "url('/static/stimuli/01_Antwerpen_S1.jpg')")
     .style("background-size", "contain")
@@ -23,48 +22,30 @@ var svg = d3.select("#scatter-plot")
     .append("g")
     .attr("transform",
         "translate(" + margin.left + "," + margin.top + ")");
-
-var x = d3.scaleLinear()
-    .domain([-100, 2400])
-    .range([0, width]);
-var y = d3.scaleLinear()
-    .domain([-100, 1800])
-    .range([height, 0]);
-
-// Add X axis
-svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
-// Add Y axis
-svg.append("g")
-    .call(d3.axisLeft(y));
-
-var globalData = []
-
-var div = d3.select("body").append("div")
+const div = d3.select("body").append("div")
     .attr("class", "tooltip")
     .style("opacity", 0);
 
 //Read the data
 d3.tsv("/static/csv/all_fixation_data_cleaned_up.csv")
     .then(data => {
-        globalData = data;
-        const filteredData = data.filter(d => d.user == "p1" && d.StimuliName == "01_Antwerpen_S1.jpg");
+        const filteredData = data.filter(d => d.StimuliName == "01_Antwerpen_S1.jpg");
         const timestamps = filteredData.map(d => d.Timestamp)
+        const users = filteredData.map(d => d.user)
             // Add dots
         svg.append('g')
             .selectAll("dot")
             .data(filteredData)
             .enter()
             .append("circle")
-            .attr("cx", d => x(d.MappedFixationPointX))
-            .attr("cy", d => y(d.MappedFixationPointY))
+            .attr("cx", d => d.MappedFixationPointX)
+            .attr("cy", d => d.MappedFixationPointY)
             .attr("r", 4)
             .on("mouseover", function(d) {
                 div.transition()
                     .duration(100)
                     .style("opacity", .9);
-                div.html(`${d.Timestamp} \n (${d.MappedFixationPointX},${d.MappedFixationPointY})`)
+                div.html(`Timestamp: ${d.Timestamp} </br> (${d.MappedFixationPointX},${d.MappedFixationPointY}) </br> User: ${d.user}`)
                     .style("left", (d3.event.pageX) + "px")
                     .style("top", (d3.event.pageY - 28) + "px");
             })
@@ -73,11 +54,6 @@ d3.tsv("/static/csv/all_fixation_data_cleaned_up.csv")
                     .duration(100)
                     .style("opacity", 0);
             })
-            .style("fill", d => {
-                //const delta = Math.floor(255 / timestamps.length);
-                //let modifier = toHex(delta * timestamps.indexOf(d.Timestamp));
-                //return `#${modifier}${modifier}${modifier}`
-                return "#000000"
-            })
+            .style("fill", "#000000")
 
     });
