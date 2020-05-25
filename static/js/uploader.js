cbe762e1a473bbe4f0e415c5cfbc70c16738459e
20192724 <j.w.v.ringelesteijn@student.tue.nl>
2020-05-25 17:27:39 +0200

    Added loading animation

--- static/js/uploader.js
+++ static/js/uploader.js
@@ -7,21 +7,26 @@ var Uploader = {};
         Upload csv with your data and all stimuli-images in one zipped file (no folders within zip). <br>
         <input type='file' accept=".zip" @change="previewFiles">
         <button @click='addStimuli()' class='btn btn-info'>Add to database</button>
+        <div v-if="uploading" class="loader"></div>
    </div>`;
 
     Uploader = Vue.component(componentName, {
         data: function() {
             return {
-                form: null
+                form: null,
+                uploading: false
             };
         },
         methods: {
             addStimuli: function() {
                 console.log('File uploading');
+                this.uploading = true;
                 $.ajax({ type: "POST", url: '/uploadzip', data: this.form, processData: false, contentType: false }).then(response => {
                     alert(`Zip uploaded successfully!`);
                     app.showDatasets();
+                    this.uploading = false;
                 });
+
             },
             previewFiles(event) {
                 let file = (event.target.files[0]);
