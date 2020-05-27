5f09be45b108c6577f456209b41f3c94587c1c54
Digital Ocean Droplet <mirchevmartin2203@gmail.com>
2020-05-27 08:06:26 +0000

    Fixing template stuff

--- config.py
+++
@@ -1,5 +0,0 @@
-class Config():
-    ZIP_UPLOAD = 'uploads'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    VUE_LINK='https://cdn.jsdelivr.net/npm/vue'
-    SECRET_KEY = str(b"\x973\xef\xdb?\x1c\xda\xb1\xe3\xa6Ho;\xf8'\xf2")
--- config_template.py
+++ config_template.py
@@ -2,4 +2,5 @@ class Config():
     ZIP_UPLOAD = 'uploads'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     VUE_LINK='https://cdn.jsdelivr.net/npm/vue'
+    SQLALCHEMY_DATABASE_URI =  ''
     SECRET_KEY = str(b"\x973\xef\xdb?\x1c\xda\xb1\xe3\xa6Ho;\xf8'\xf2")
