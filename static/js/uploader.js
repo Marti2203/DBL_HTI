1d40fe1281a52673127ac5918513d0a59c7a65ef
Martin Mirchev <mirchevmartin2203@gmail.com>
2020-06-04 17:19:30 +0200

    Removed upload button by mistake

--- static/js/uploader.js
+++ static/js/uploader.js
@@ -5,6 +5,7 @@ var Uploader = {};
     const template = `
     <div id='${componentName}-root'>
         <i style="font-size:13px">Upload a zip file containing a csv with your data and all stimuli-images. Do not use folders within the zipped file.</i><br> <br>
+        <input type='file' accept=".zip" @change="previewFiles">
         <button @click='addStimuli()' class='btn btn-info' :disabled="uploading">Add to database</button>
         <div v-if="uploading" class="loader"></div>
    </div>`;
@@ -22,14 +23,14 @@ var Uploader = {};
                 this.uploading = true;
                 $.ajax({ type: "POST", url: '/uploadzip', data: this.form, processData: false, contentType: false })
                     .then(response => {
-                    alert(`Zip uploaded successfully!`);
-                    app.showDatasets();
+                        alert(`Zip uploaded successfully!`);
+                        app.showDatasets();
                         this.uploading = false;
                     })
                     .catch(e => {
                         console.log(e);
                         this.uploading = false;
-                });
+                    });
             },
             previewFiles(event) {
                 let file = (event.target.files[0]);
