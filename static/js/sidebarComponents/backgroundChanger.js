ba511b748bed45524b052d8440de857096dee67a
Isa Dantuma <i.l.g.dantuma@gmail.com>
2020-06-13 14:36:19 +0200

    Start in backgroundchange component

---
+++ static/js/sidebarComponents/backgroundChanger.js
@@ -0,0 +1,37 @@
+'use strict';
+var StimuliSelector = {};
+(() => {
+    const componentName = 'background-changer';
+    const template = `
+    <div id='${componentName}-root'>
+        <input type="checkbox" id="addbg" v-model="isBackgroundVisible"/>
+        <label for="addbg"> Add stimulus as background</label>
+    </div>`;
+
+    BackgroundChanger = Vue.component(componentName, {
+        created: function() {
+            this.$emit('created', this);
+        },
+
+        data: function() {
+            return {
+                isBackgroundVisible: true,
+            };
+        },
+
+        watch: {
+            isBackgroundVisible: function(value) {
+                if(!value) {
+                    const graphic = d3.select(`#${componentName}-graphic`);
+                    graphic.style('background-image', ``);
+                }
+                else{
+                    const url = `/uploads/stimuli/${app.datasetName}/${this.selectedStimuli}`;
+                    const graphic = d3.select(`#${componentName}-graphic`);
+                    graphic.style('background-image', `url('${url}')`);
+                }
+            }
+        },
+
+    });
+})();
